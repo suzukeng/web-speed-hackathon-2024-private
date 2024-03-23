1f52408b8a1678179c26cc4a016cc9db5aab8276
suzuken <suzuken93@gmail.com>
2024-03-24 01:09:41 +0900

    不要なデータの削減

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -59,9 +59,9 @@ export default defineConfig(async (): Promise<Options[]> => {
       platform: 'browser',
       shims: true,
       sourcemap: 'inline',
-      splitting: false,
+      splitting: true,
       target: ['chrome123'],
-      treeshake: false,
+      treeshake: true,
     },
   ];
 });
