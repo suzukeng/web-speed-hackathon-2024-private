018653db75ccdbd71ff918ab00a14f8dd69566ed
suzuken <suzuken93@gmail.com>
2024-03-24 02:23:14 +0900

    クエリ回数削減

--- workspaces/app/src/features/book/components/BookListItem.tsx
+++ workspaces/app/src/features/book/components/BookListItem.tsx
@@ -9,7 +9,7 @@ import { Spacer } from '../../../foundation/components/Spacer';
 import { Text } from '../../../foundation/components/Text';
 import { useImage } from '../../../foundation/hooks/useImage';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
-import { useBook } from '../hooks/useBook';
+// import { useBook } from '../hooks/useBook';
 
 const _Wrapper = styled.li`
   width: 100%;
@@ -28,12 +28,10 @@ const _ImgWrapper = styled.div`
 `;
 
 type Props = {
-  bookId: string;
+  book: any;
 };
 
-export const BookListItem: React.FC<Props> = ({ bookId }) => {
-  const { data: book } = useBook({ params: { bookId } });
-
+export const BookListItem: React.FC<Props> = ({ book }) => {
   const imageUrl = useImage({ height: 64, imageId: book.image.id, width: 64 });
 
   return (
